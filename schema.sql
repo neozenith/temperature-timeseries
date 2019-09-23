3365fabcbad5a13d93ecc8bea3f6810ee667084c
Josh Peak <neozenith.dev@gmail.com>
2019-09-23 20:59:30 +1000

    Attempt to add indexes

--- schema.sql
+++ schema.sql
@@ -8,3 +8,7 @@ CREATE TABLE IF NOT EXISTS metrics
   ts_iso8601 TEXT NOT NULL,
   value REAL
 );
+
+-- CREATE INDEX IF NOT EXISTS metric_name ON metrics (metric);
+-- CREATE INDEX IF NOT EXISTS metric_ts ON metrics (ts);
+-- CREATE INDEX IF NOT EXISTS metric_value ON metrics (value);
