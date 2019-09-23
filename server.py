67190bd4f37116f3ffcf9e3e9249784822498f8c
Josh Peak <neozenith.dev@gmail.com>
2019-09-23 20:26:53 +1000

    Add basic web interface

--- server.py
+++ server.py
@@ -1,13 +1,44 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask import current_app, g
+import sqlite3
+import logging
+
+log = logging.getLogger(__name__)
 
 app = Flask(__name__)
 
-@app.route('/')
 
+def get_db():
+    if "db" not in g:
+        g.db = sqlite3.connect("data.db", detect_types=sqlite3.PARSE_DECLTYPES)
+        g.db.row_factory = sqlite3.Row
+
+    return g.db
+
+
+def close_db(e=None):
+    db = g.pop("db", None)
+
+    if db is not None:
+        db.close()
+
+
+@app.route("/")
 def index():
+    output = "<html><body>"
+    for stat in stats():
+        for s in stat:
+            output += f"<div>{s}</div>"
+    output += "</body></html>"
+    return output
 
- return 'Hello world'
 
-if __name__ == '__main__':
+def stats():
+    conn = get_db()
+    results = conn.execute("SELECT count(*), max(ts_iso8601) FROM metrics;")
+    return [r for r in results]
 
- app.run(debug=True, host='0.0.0.0')
+
+if __name__ == "__main__":
+
+    app.run(debug=True, host="0.0.0.0")
