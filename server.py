dea6aeb901c899ef2c4c048794eba2e6f34be1d2
Josh Peak <neozenith.dev@gmail.com>
2019-09-28 17:01:11 +1000

    Add last day stats and queryable API for all metrics and specific metrics

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template
-from flask import current_app, g
-from flask import jsonify
 import sqlite3
+import datetime
+import time
 import logging
+
+from flask import Flask, request, g
+from flask import jsonify
 
 log = logging.getLogger(__name__)
 
@@ -31,25 +33,86 @@ def index():
 
 @app.route("/metrics")
 def metrics():
-    return jsonify(stats())
+    now = datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc)
+    start = request.args.get("from", default=None, type=int)
+    end = request.args.get("to", default=None, type=int)
+
+    if start is None:
+        start = (now - datetime.timedelta(days=1)).timestamp()
+    else:
+        start = (now - datetime.timedelta(days=start)).timestamp()
+
+    if end is None:
+        end = now.timestamp()
+    else:
+        end = (now - datetime.timedelta(days=end)).timestamp()
+    return jsonify(stats(metric=None, start=start, end=end))
 
 
-def stats():
+@app.route("/metric/<string:metric>/")
+def get_metric(metric):
+    now = datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc)
+    start = request.args.get("from", default=None, type=int)
+    end = request.args.get("to", default=None, type=int)
+
+    if start is None:
+        start = (now - datetime.timedelta(days=1)).timestamp()
+    else:
+        start = (now - datetime.timedelta(days=start)).timestamp()
+
+    if end is None:
+        end = now.timestamp()
+    else:
+        end = (now - datetime.timedelta(days=end)).timestamp()
+
+    return jsonify(stats(metric, start, end))
+
+
+def stats(metric=None, start=None, end=None):
     conn = get_db()
-    results = conn.execute(
-        """
-        SELECT
-            metric,
-            min(value) as value_min,
-            avg(value) as value_avg,
-            max(value) as value_max,
-            max(ts) as ts,
-            count(*) as count,
-            max(ts_iso8601) as ts_iso8601
-        FROM metrics
-        GROUP BY metric;
-        """
-    )
+    now = datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc)
+    last_day = (now - datetime.timedelta(days=1)).timestamp()
+
+    if metric is not None:
+        results = conn.execute(
+            """
+            SELECT
+                id, ts, metric, value
+            FROM metrics
+            WHERE metric = ? AND ts >= ? AND ts <= ?
+            ORDER BY ts DESC;
+            """,
+            [metric, start, end],
+        )
+    elif start is not None and end is not None:
+        results = conn.execute(
+            """
+            SELECT
+                id, ts, metric, value
+            FROM metrics
+            WHERE ts >= ? AND ts <= ?
+            ORDER BY ts DESC;
+            """,
+            [start, end],
+
+        )
+    else:
+        results = conn.execute(
+            """
+            SELECT
+                metric,
+                count(*) as count,
+                min(value) as value_min,
+                avg(value) as value_avg,
+                max(value) as value_max
+            FROM metrics
+            WHERE ts >= ?
+            GROUP BY metric
+            """,
+            [last_day],
+
+        )
+
     return [{k: r[k] for k in r.keys()} for r in results]
 
 
