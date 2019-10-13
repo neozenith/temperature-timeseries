49fcb0bea08118780f93b4e6f86c96a5e0a0a2d0
Josh Peak <neozenith.dev@gmail.com>
2019-10-13 22:15:05 +1100

    shuffle prod and dev dependencies

--- start.sh
+++ start.sh
@@ -1,15 +1,16 @@
 #! /bin/bash
+date
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 START_DIR="$(pwd)"
 
-echo -e "SCRIPT DIR: $(SCRIPT_DIR)"
-echo -e "START DIR: $(START_DIR)"
+echo -e "SCRIPT DIR: ${SCRIPT_DIR}"
+echo -e "START DIR: ${START_DIR}"
 
 cd $SCRIPT_DIR
-
+date
 source .venv/bin/activate
-pip install -r .deps/requirements-dev.txt
-
+pip install -r .deps/requirements.txt
+date
 if [ -z "$(ps u | grep logger.py | grep -v grep)" ]; then
   python3 logger.py &
 else
