cc522a70ecb4d50ad901fdbbb45adfac751aa02c
Josh Peak <neozenith.dev@gmail.com>
2019-10-13 22:05:06 +1100

    auto switch to script dir

--- start.sh
+++ start.sh
@@ -1,16 +1,22 @@
 #! /bin/bash
-cd /home/pi/projects/temperature-timeseries
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+START_DIR="$(pwd)"
+
+echo -e "SCRIPT DIR: $(SCRIPT_DIR)"
+echo -e "START DIR: $(START_DIR)"
+
+cd $SCRIPT_DIR
 
 source .venv/bin/activate
 pip install -r .deps/requirements-dev.txt
 
-if [ -z "$(ps aux | grep logger.py | grep -v grep)" ]; then
+if [ -z "$(ps u | grep logger.py | grep -v grep)" ]; then
   python3 logger.py &
 else
   echo "Already running logger.py"
 fi
 
-if [ -z "$(ps aux | grep gunicorn | grep -v grep)" ]; then
+if [ -z "$(ps u | grep gunicorn | grep -v grep)" ]; then
   gunicorn -w 2 -b 0.0.0.0:8000 server:app &
 else 
   echo "Already running server.py"
